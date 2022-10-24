f0d8ab4e3379d78b2cbf69df4e46577efacf93c4
Yoshiaki Iwamoto <yoshiaki.iwamoto@gmail.com>
2022-10-24 18:40:31 +0900

    L3

--- real-world-vue/src/App.vue
+++ real-world-vue/src/App.vue
@@ -3,6 +3,7 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
+  <h1>Events for Good</h1>
   <router-view />
 </template>
 
@@ -26,5 +27,8 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+h4{
+  font-size: 20px;
 }
 </style>
